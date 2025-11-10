bfea3c531b92cf20a60cbb016e359b60c335d1f1
Hassan Mirza <hassanmirza@outlook.be>
2025-11-10 20:11:50 +0100

    Updated headers

--- headers.php
+++ headers.php
@@ -2,6 +2,11 @@
 
 function set_json_header()
 {
-    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
 }
-
